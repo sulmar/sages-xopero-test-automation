29e8984c5e9229d1005b8cc11e9aeb6ea2c413cd
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-01-08 17:12:44 +0100

    Blokowanie konta - UserAccount z mechanizmem bezpieczeństwa
    - Dodano klasę UserAccount z mechanizmem blokowania konta
    - Śledzenie nieudanych prób logowania przez FailedLoginAttempts
    - Blokowanie konta po przekroczeniu limitu (MaxLoginAttempts = 3)
    - Właściwość IsLocked z private set do kontroli stanu konta
    - Rzucanie InvalidOperationException przy próbie logowania na zablokowane konto
    - Reset licznika przy udanym logowaniu
    - Naprawiono błąd logiczny w warunku walidacji hasła (password != _password)

---
+++ src/BackupSystem.Core/UserAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupSystem.Core;
+
+public class UserAccount
+{
+    private readonly string _login;
+    private readonly string _password;
+
+    public bool IsLocked { get; private set; }
+    public int FailedLoginAttempts { get; private set; }
+
+    public const int MaxLoginAttempts = 3;
+
+    public UserAccount(string login, string password)
+    {
+        _login = login;
+        _password = password;
+    }
+
+    public void Login(string login, string password)
+    {
+        if (IsLocked)
+            throw new InvalidOperationException("Account is locked.");
+
+        if (login != _login || password != _password)
+        {
+            FailedLoginAttempts++;
+
+            if (FailedLoginAttempts >= MaxLoginAttempts) 
+                IsLocked = true;
+
+            return;
+        }
+
+        FailedLoginAttempts = 0;
+    }
+}
