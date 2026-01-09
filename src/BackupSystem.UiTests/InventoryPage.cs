edb8833461c893ac0ffdbd334eeaa34f1e8aca17
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-01-09 17:14:57 +0100

    feat(ui-tests): dodanie strony Inventory i testów koszyka
    - Dodano klasę InventoryPage implementującą Page Object Model
    
    - Dodano metodę AddBackpackToCart() do dodawania produktu do koszyka
    
    - Dodano metodę CartItemCount() do sprawdzania liczby produktów w koszyku
    
    - Dodano test weryfikujący aktualizację badge'a koszyka po dodaniu produktu

---
+++ src/BackupSystem.UiTests/InventoryPage.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Playwright;
+
+namespace BackupSystem.UiTests;
+
+public class InventoryPage
+{
+    private readonly IPage page;
+
+    public InventoryPage(IPage page)
+    {
+        this.page = page;
+    }
+
+    public async Task<int> CartItemCount()
+    {
+        return await GetCartBadgeCount();
+    }
+
+    public async Task AddBackpackToCart()
+    {
+        await page.ClickAsync("[data-test='add-to-cart-sauce-labs-backpack']");
+    }
+
+    private async Task<int> GetCartBadgeCount()
+    {
+        if (!await page.IsVisibleAsync(".shopping_cart_badge"))
+            return 0;
+
+        var text = await page.InnerTextAsync(".shopping_cart_badge");
+        return int.Parse(text);
+    }
+}
---
+++ src/BackupSystem.UiTests/InventoryPageTests.cs
@@ -0,0 +1,17 @@
+﻿namespace BackupSystem.UiTests;
+
+public class InventoryPageTests : BasePageTests
+{
+    [Fact]
+    public async Task AddToCart_WhenProductAdded_ShouldUpdateCartBadge()
+    {
+        // Arrange
+        var inventoryPage = new InventoryPage(page);
+
+        // Act
+        await inventoryPage.AddBackpackToCart();
+
+        // Assert
+        Assert.Equal(1, await inventoryPage.CartItemCount());
+    }
+}
